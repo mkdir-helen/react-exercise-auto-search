86ca2c46cefaf2283adca5757e9d26bdabd2f363
mkdir-helen <experiment.helen@gmail.com>
2018-12-07 19:59:57 -0500

    Added a word list

--- src/words.js
+++ src/words.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-const dictionary = ["apple", "ain't", "angular", "aries", "anastasia", "ahjuma", "asap", "argon", "anabel"];
+const dictionary = ["apple", "angel", "angular", "aries", "anastasia", "ahjuma", "asap", "argon", "anabel",
+"blueberry", "bathroom", "betty", "bob", "brunette", "blonde", "baby", "bicycle", "banana", "butt", "but", 
+"cat", "dog", "fish", "usher", "jay-z", "joker", "joking", "johnny", "john", "george", "greg", "ian", "icecream",
+"alex", "react", "javascript", "redux", "express", "coffee", "jeff", "jonathan", "chris", "car", "christmas", 
+"xylophone", "xmas", "x-man", "x-ray", "fu", "gah", "amelia", "ed", "education", "meteor", "collin", "helen", 
+"melon", "rabbit", "rabbit-hole", "riddle", "travis", "train", "marta", "digitalcrafts", "docker", "go", "go-lang",
+"ear", "earwax", "snot", "pooh", "poop", "hyper", "drunk", "driver", "daniel", "michael", "mickey", "minestrone",
+"nat", "nathan", "natalie", "node", "none", "null", "omg", "rofl", "lol", "lolipop", "lies", "truth", "stan", "steve",
+"carlos", "fat", "freak", "frizzle", "gangster", "gungho", "grades", "git", "github", "italy", "iam", "isee", 
+"iphone", "dork", "dandruff", "dance", "vue", "vacuum", "van", "virus", "victor", "villain", "zack", "zesty", 
+"why", "who", "what", "where", "how", "when", "kite", "kroger", "chicken", "beef", "buy", "kick", "kickass", 
+"lonely", "lick", "lifecycle", "css", "html", "jquery", "question", "quest", "qtip", "query", "promise", "pee",
+"peter", "pisces", "sagittarius", "aquarius", "taurus", "teacher", "tom", "rob", "todo", "silence", "superb",
+"suck", "sass", "saas", "paas", "tech", "tyrant", "clare", "jason", "json", "anokhee", "lorenzo", "beth", "brien",
+"quinton", "isaac", "evan", "sam", "will", "kyle", "zakia", "treason", "treachery", "tmi", "hotstuff", "honey", 
+"whore", "hoe", "haha", "udemy", "unfriendly", "uh-huh", "underrated", "yarn", "yawn", "yoda", "yuck", "yale", "yonder",
+"zebra", "zeit", "aws", "django", "direct", "mkdir", "gatsby", "php", "postgres", "sql", "mongodb", "enjoy",
+"oh", "ohmygah", "oreos", "ohyeah" 
+];
 
 export default dictionary;
